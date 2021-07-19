1ef87e4c869c065e20e155cae2498db4d55418fd
Kunshang Ji <kunshang.ji@intel.com>
2021-07-19 21:19:04 +0800

    impl convert method

--- java/ciderJNI/src/main/native/com_mapd_CiderJNI.cpp
+++ java/ciderJNI/src/main/native/com_mapd_CiderJNI.cpp
@@ -51,12 +51,64 @@ std::string random_string(std::size_t length) {
   return random_string;
 }
 
-void convertToArrowTable(std::shared_ptr<arrow::Table>,
+std::shared_ptr<arrow::Schema> parse_to_schema(std::string json_schema) {
+  std::vector<std::shared_ptr<arrow::Field>> fields;
+  rapidjson::Document d;
+  if (d.Parse(json_schema).HasParseError() || !d.HasMember("Columns")) {
+    LOG(ERROR) << "invalid json for schema!";
+  }
+  const rapidjson::Value& columns = d["Columns"];
+  std::string schema;
+  for (rapidjson::Value::ConstValueIterator v_iter = columns.Begin();
+       v_iter != columns.End();
+       ++v_iter) {
+    const rapidjson::Value& field = *v_iter;
+    for (rapidjson::Value::ConstMemberIterator m_iter = field.MemberBegin();
+         m_iter != field.MemberEnd();
+         ++m_iter) {
+      const char* col_name = m_iter->name.GetString();
+      const char* col_type = m_iter->value.GetString();
+      std::string col_type_str(col_type);
+      std::shared_ptr<arrow::DataType> type;
+      if (col_type_str.compare("int") == 0)
+        type = arrow::int32();
+      else if (col_type_str.compare("long") == 0)
+        type = arrow::int64();
+      else if (col_type_str.compare("float") == 0)
+        type = arrow::float32();
+      else if (col_type_str.compare("double") == 0)
+        type = arrow::float64();
+      else
+        LOG(WARNING) << "unsupported type: " << col_type_str;
+
+      auto f = std::make_shared<arrow::Field>(std::string(col_name), type);
+      fields.push_back(f);
+    }
+  }
+  std::shared_ptr<arrow::Schema> s = std::make_shared<arrow::Schema>(fields);
+
+  return s;
+}
+
+void convertToArrowTable(std::shared_ptr<arrow::Table> table,
                          int64_t* dataBuffers,
                          int64_t* nullBuffers,
-                         std::string schema) {
+                         std::string schema,
+                         int rowCount) {
+  // todo:: null buffer???
+  auto s = parse_to_schema(schema);
+  std::vector<std::shared_ptr<arrow::Array>> arrays;
+  for (int i = 0; i < s->fields().size(); i++) {
+    auto buffer = std::make_shared<arrow::Buffer>((uint8_t*)(dataBuffers + i), rowCount);
+    auto data = arrow::ArrayData::Make(s->fields()[i]->type(), rowCount, {buffer});
+    auto array = arrow::MakeArray(data);
+    arrays.push_back(array);
+  }
+
+  table = arrow::Table::Make(s, arrays, rowCount);
   return;
 }
+
 }  // namespace util
 /*
  * Class:     com_mapd_CiderJNI
@@ -95,13 +147,13 @@ JNIEXPORT jint JNICALL Java_com_mapd_CiderJNI_processBlocks(JNIEnv* env,
   std::shared_ptr<arrow::Table> arrowTable;
   const char* schemaPtr = env->GetStringUTFChars(schema, nullptr);
   std::string tableSchema(schemaPtr);
-  util::convertToArrowTable(arrowTable, dataValuesPtr, dataNullsPtr, schemaPtr);
+  util::convertToArrowTable(arrowTable, dataValuesPtr, dataNullsPtr, schemaPtr, rowCount);
   dbe->importArrowTable("test", arrowTable);
 
   const char* sqlPtr = env->GetStringUTFChars(sql, nullptr);
   std::string queryInfo = "execute relalg " + std::string(sqlPtr);
   auto res = dbe->executeRA(queryInfo);
-  
+
   /*
   {
     // new a CiderEntry
