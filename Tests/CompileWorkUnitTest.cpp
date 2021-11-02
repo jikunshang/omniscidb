a43b8211d1bd19415aba3af73594a09e18502962
Kunshang Ji <kunshang.ji@intel.com>
2021-11-02 16:57:04 +0800

    construct parameters and verify

--- Tests/CompileWorkUnitTest.cpp
+++ Tests/CompileWorkUnitTest.cpp
@@ -31,33 +31,97 @@
 #define BASE_PATH "./tmp"
 #endif
 
-// RelAlgExecutionUnit buildFakeRelAlgEU() {
-//
-// }
+ RelAlgExecutionUnit buildFakeRelAlgEU() {
+  RelAlgExecutionUnit ra_exe_unit{};
+
+   int table_id = 100;
+   int column_id_0 = 1;
+   int column_id_1 = 2;
+
+   // input_descs
+   std::vector<InputDescriptor> input_descs;
+   InputDescriptor input_desc_0(table_id, 0);
+   input_descs.push_back(input_desc_0);
+   ra_exe_unit.input_descs = input_descs;
+
+   // input_col_descs
+   std::list<std::shared_ptr<const InputColDescriptor>> input_col_descs;
+   std::shared_ptr<const InputColDescriptor> input_col_desc_0 =
+       std::make_shared<const InputColDescriptor>(column_id_0, table_id, 0);
+   std::shared_ptr<const InputColDescriptor> input_col_desc_1 =
+       std::make_shared<const InputColDescriptor>(column_id_1, table_id, 0);
+   input_col_descs.push_back(input_col_desc_0);
+   input_col_descs.push_back(input_col_desc_1);
+   ra_exe_unit.input_col_descs = input_col_descs;
+
+   // simple_quals
+   SQLTypes sqlTypes{SQLTypes::kBOOLEAN};
+   SQLTypes subtypes{SQLTypes::kNULLT};
+   SQLTypes dateSqlType{SQLTypes::kDATE};
+   SQLTypes longTypes{SQLTypes::kBIGINT};
+
+   SQLTypeInfo date_col_info(dateSqlType, 0,0,false,EncodingType::kENCODING_DATE_IN_DAYS, 0, subtypes);
+   std::shared_ptr<Analyzer::ColumnVar> col1 = std::make_shared<Analyzer::ColumnVar>(date_col_info, table_id, column_id_0, 0);
+   SQLTypeInfo ti_boolean(sqlTypes, 0, 0, false, EncodingType::kENCODING_NONE, 0, subtypes);
+   SQLTypeInfo ti_long(longTypes, 0, 0, false, EncodingType::kENCODING_NONE, 0, subtypes);
+
+   std::shared_ptr<Analyzer::Expr> leftExpr = std::make_shared<Analyzer::UOper>(date_col_info, false, SQLOps::kCAST, col1);
+   Datum d;
+   d.intval = 757382400;
+   std::shared_ptr<Analyzer::Expr> rightExpr = std::make_shared<Analyzer::Constant>(dateSqlType, false, d);
+   std::shared_ptr<Analyzer::Expr> simple_qual_0 =
+       std::make_shared<Analyzer::BinOper>(ti_boolean, false, SQLOps::kGE, SQLQualifier::kONE, leftExpr, rightExpr);
+   std::list<std::shared_ptr<Analyzer::Expr>> simple_quals;
+   simple_quals.push_back(simple_qual_0);
+
+   ra_exe_unit.simple_quals = simple_quals;
+
+   std::vector<Analyzer::Expr*> target_exprs;
+   Analyzer::ColumnVar* target_expr_0 = new Analyzer::ColumnVar(ti_long, table_id, column_id_1, 0);
+
+   target_exprs.push_back(target_expr_0);
+
+   ra_exe_unit.target_exprs = target_exprs;
+   return ra_exe_unit;
+ }
 
 TEST(BuildEU, EU1) {
   ASSERT_TRUE(true);
-  auto executor = Executor::getExecutor(Executor::UNITARY_EXECUTOR_ID);
+  auto executor = Executor::getExecutor(Executor::UNITARY_EXECUTOR_ID);  // step1
   CompilationResult compilation_result;
   std::unique_ptr<QueryMemoryDescriptor> query_mem_desc;
 
   // needed parameters
   std::vector<InputTableInfo> query_infos;
+  Fragmenter_Namespace::FragmentInfo fi_0;
+  fi_0.fragmentId = 0;
+  fi_0.shadowNumTuples = 20;
+  fi_0.physicalTableId = 100;
+  fi_0.setPhysicalNumTuples(20);
+
+  Fragmenter_Namespace::TableInfo ti_0 ;
+  ti_0.fragments = {fi_0};
+  ti_0.setPhysicalNumTuples(20);
+
+  InputTableInfo iti_0{100, ti_0};
+  query_infos.push_back(iti_0);
+
   PlanState::DeletedColumnsMap deleted_cols_map;
-  RelAlgExecutionUnit ra_exe_unit{};
-  CompilationOptions co;
+  CompilationOptions co = CompilationOptions::defaults(ExecutorDeviceType::CPU);
   ExecutionOptions eo;
   CudaMgr_Namespace::CudaMgr* cuda_mgr = nullptr;
   bool allow_lazy_fetch{false};
   std::shared_ptr<RowSetMemoryOwner> row_set_mem_owner = nullptr;
   size_t max_groups_buffer_entry_guess {0};
-  int8_t crt_min_byte_width {0} ;
+  int8_t crt_min_byte_width {MAX_BYTE_WIDTH_SUPPORTED} ;
   bool has_cardinality_estimation{false};
   ColumnCacheMap column_cache;
   RenderInfo* render_info = nullptr;
 
+  RelAlgExecutionUnit ra_exe_unit = buildFakeRelAlgEU();
+
   // this method will fail currently :(
-  std::tie(compilation_result, query_mem_desc) =
+  std::tie(compilation_result, query_mem_desc) =   // step 2
       executor->compileWorkUnit(query_infos,
                                 deleted_cols_map,
                                 ra_exe_unit,
@@ -71,6 +135,11 @@ TEST(BuildEU, EU1) {
                                 has_cardinality_estimation,
                                 column_cache,
                                 render_info);
+  long l = 0;
+  // step 3
+//  createKernel()
+//
+//  runWithInput()
 }
 
 int main() {
